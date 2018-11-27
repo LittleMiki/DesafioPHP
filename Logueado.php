6143a787f0a5274c44fe7df977d64551f8451b17
Miguel Angel <troodycdaw@gmail.com>
2018-11-27 13:50:16 +0100

    version 1.3

--- Logueado.php
+++ Logueado.php
@@ -11,24 +11,52 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            require_once 'Conexion.php';
-            require_once 'Usuario.php';
-            session_start();
-            $co = new Conexion(Constantes::$IP, Constantes::$USUARIO, Constantes::$PASS, Constantes::$BBDD);
-            $info = getdate();
-            $aulas= $co->devolverAulas();
-            $fechaMinima = $info['year'].'-'.$info['mon'].'-'.$info['mday'];
-            $a = $_SESSION['usuario'];
-            $co = new Conexion('localhost', 'daw206', 'daw206', 'desafioPHP');
-            $u = $co->devolverUsuario($a);
-            echo '<h1>Bienvenido ' . $u->getCorreo() . '</h1> ';
-            echo "<div> <img src='".$u->getFoto()."'></div>";
-            echo "<input type='date' name='calendario' min='".$fechaMinima."' value='".$fechaMinima."'>";
-            echo "<select>";
-            for($i=0;$i<count($aulas);$i++){
-                echo "<option>".$aulas[$i]."</option>";
+        require_once 'Conexion.php';
+        require_once 'Usuario.php';
+        require_once 'Reserva.php';
+        session_start();
+        $co = new Conexion(Constantes::$IP, Constantes::$USUARIO, Constantes::$PASS, Constantes::$BBDD);
+        $info = getdate();
+        $aulas = $co->devolverAulas();
+        $reservas = $_SESSION['reservas'];
+        $fechaMinima = $info['year'] . '-' . $info['mon'] . '-' . $info['mday'];
+        $a = $_SESSION['usuario'];
+        $co = new Conexion('localhost', 'daw206', 'daw206', 'desafioPHP');
+        $u = $co->devolverUsuario($a);
+        echo '<h1>Bienvenido ' . $u->getCorreo() . '</h1> ';
+        echo "<div> <img src='" . $u->getFoto() . "'></div>";
+
+        echo "<form method='POST' action='Controlador.php'>";
+
+        echo "<input type='date' name='fecha' min='" . $fechaMinima . "' value='" . $fechaMinima . "'>";
+        echo "<select name='aula'>";
+        for ($i = 0; $i < count($aulas); $i++) {
+            echo "<option>" . $aulas[$i] . "</option>";
+        }
+        echo "</select>";
+        echo "<input type='text' hidden='' name= 'correo' value='" . $u->getCorreo() . "'>";
+        echo "<input type='submit' name= 'reservas' value='seleccionar'>";
+        echo "</form>";
+
+
+        if ($reservas != null) {
+            echo "<div align= 'left'>";
+            echo "<h3>Horario</h3>";
+            for ($i = 0; $i < 6; $i++) {
+                echo "<form method = 'POST' action='Controlador.php'>";
+                for($e = 0; $e < count($reservas); $e++){
+                    if ($reservas[$e]->getHora() == $i){
+                        echo "<input type='text' name='correo' value='".$reservas[$e]->getCorreo()."'>";
+                    }else{
+                        echo "<input type='text' name='correo' value='Libre'>";
+                        echo "<input type='submit' name='reservas' value='reservar'>";
+                    }
+                }
+                
+                echo "</form>";
             }
-            echo "</select>";
+            echo "</div>";
+        }
         ?>
     </body>
 </html>
