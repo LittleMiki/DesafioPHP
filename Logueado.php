67d4b675891bb2c0054fab826043782b9f19281f
Miguel Angel <troodycdaw@gmail.com>
2018-11-27 12:55:13 +0100

    version 1.2

--- Logueado.php
+++ Logueado.php
@@ -14,11 +14,21 @@ and open the template in the editor.
             require_once 'Conexion.php';
             require_once 'Usuario.php';
             session_start();
+            $co = new Conexion(Constantes::$IP, Constantes::$USUARIO, Constantes::$PASS, Constantes::$BBDD);
+            $info = getdate();
+            $aulas= $co->devolverAulas();
+            $fechaMinima = $info['year'].'-'.$info['mon'].'-'.$info['mday'];
             $a = $_SESSION['usuario'];
             $co = new Conexion('localhost', 'daw206', 'daw206', 'desafioPHP');
             $u = $co->devolverUsuario($a);
             echo '<h1>Bienvenido ' . $u->getCorreo() . '</h1> ';
             echo "<div> <img src='".$u->getFoto()."'></div>";
+            echo "<input type='date' name='calendario' min='".$fechaMinima."' value='".$fechaMinima."'>";
+            echo "<select>";
+            for($i=0;$i<count($aulas);$i++){
+                echo "<option>".$aulas[$i]."</option>";
+            }
+            echo "</select>";
         ?>
     </body>
 </html>
